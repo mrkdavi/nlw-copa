47d3b29400c9bf4a5bb62eba771d42a466e0a4a6
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-11-02 17:27:43 -0300

    Day 03: client-side mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,10 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import { AuthContextProvider } from './src/context/AuthContext';
+
 import { SignIn } from './src/screens/SignIn';
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -11,12 +14,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      { fontsLoaded ? <SignIn /> : <Loading /> }
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Pools /> : <Loading /> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
